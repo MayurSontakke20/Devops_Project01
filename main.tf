c009e4626fde8d78f23a66e7af04fc827590d384
MayurSontakke20 <124254352+MayurSontakke20@users.noreply.github.com>
2023-04-08 14:29:49 +0530

    done

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
 region = "ap-south-1"
-access_key = "AKIARSPNELGYCJEVYJ4K"
-secret_key = "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"
+access_key = "AKIA2GYURA2KUPRQHTUG"
+secret_key = "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"
 }
 
 resource "aws_instance" "one" {
-  ami             = "ami-0d81306eddc614a45"
+  ami             = "ami-06e6b44dd2af20ed0"
   instance_type   = "t2.micro"
-  key_name        = "rmk8s"
+  key_name        = "newkey"
   vpc_security_group_ids = [aws_security_group.three.id]
   availability_zone = "ap-south-1a"
   user_data       = <<EOF
@@ -24,9 +24,9 @@ EOF
 }
 
 resource "aws_instance" "two" {
-  ami             = "ami-0d81306eddc614a45"
+  ami             = "ami-06e6b44dd2af20ed0"
   instance_type   = "t2.micro"
-  key_name        = "rmk8s"
+  key_name        = "newkey"
   vpc_security_group_ids = [aws_security_group.three.id]
   availability_zone = "ap-south-1b"
   user_data       = <<EOF
